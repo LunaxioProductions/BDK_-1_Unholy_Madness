7ae65879b35e5a519ca1ad6b262f76738fb44176
Lunaxio <67171912+LunaxioProductions@users.noreply.github.com>
2024-01-17 10:29:08 -0500

    Update GalacticGreaves.cs

--- Content/Items/Armor/GalacticGreaves.cs
+++ Content/Items/Armor/GalacticGreaves.cs
@@ -8,7 +8,7 @@ namespace TouHouChaos.Content.Items.Armor
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Legs value here will result in TML expecting a X_Legs.png file to be placed next to the item's main texture.
 	[AutoloadEquip(EquipType.Legs)]
-	public class ExampleLeggings : ModItem
+	public class GalacticLeggings : ModItem
 	{
 		public static readonly int MoveSpeedBonus = 20;
 
